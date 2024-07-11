0531d1d22f748f27aa34e286a2dc8d67a74c8f0f
sneha1803 <139860645+sneha1803@users.noreply.github.com>
2024-07-11 13:10:04 +0530

    Update food_order_sql_code.sql

--- food_order_sql_code.sql
+++ food_order_sql_code.sql
@@ -11,7 +11,7 @@ ALTER TABLE food_orders MODIFY COLUMN Order_Time TIME;
 ALTER TABLE food_orders MODIFY COLUMN Delivery_Time TIME;
 
 
--- Separate out the Discount type and Discount Amount from Discounts_and_Offers coulumn--
+-- Separate out the Discount type Amount from Discounts_and_Offers coulumn--
 SELECT DISTINCT Discounts_and_Offers FROM food_orders ;
 ALTER TABLE food_orders ADD COLUMN Discount_Type VARCHAR(20);
 UPDATE food_orders SET Discount_Type='ON APP' WHERE Discounts_and_Offers LIKE '%On App%';
@@ -20,12 +20,14 @@ UPDATE food_orders SET Discount_Type='PROMO' WHERE Discounts_and_Offers LIKE '%o
 UPDATE food_orders SET Discount_Type='NONE' WHERE Discounts_and_Offers LIKE 'None';
 UPDATE food_orders SET Discount_Type='BASIC' WHERE Discount_Type IS NULL;
 
+-- Separate out the Discount Amount in % from Discounts_and_Offers coulumn--
 UPDATE food_orders SET Discounts_and_Offers='0.05' WHERE Discounts_and_Offers LIKE '%On App%';
 UPDATE food_orders SET Discounts_and_Offers='0.15' WHERE Discounts_and_Offers LIKE '%New User%';
 UPDATE food_orders SET Discounts_and_Offers='0.10' WHERE Discounts_and_Offers LIKE '10%';
 UPDATE food_orders SET Discounts_and_Offers='50' WHERE Discounts_and_Offers LIKE '%off Promo%';
 UPDATE food_orders SET Discounts_and_Offers='0' WHERE Discounts_and_Offers LIKE 'None';
 
+-- Separate out the Discount Amount in Rupees from Discounts_and_Offers coulumn--
 ALTER TABLE food_orders ADD COLUMN Discount_Amount INT;
 UPDATE food_orders SET Discount_Amount=0.05*Order_Value WHERE Discount_Type LIKE '%ON APP%';
 UPDATE food_orders SET Discount_Amount=0.1*Order_Value WHERE Discount_Type LIKE '%BASIC%';
@@ -54,15 +56,3 @@ UPDATE food_orders SET Profit_Margin = Profit/Revenue ;
 -- v. Commission Percentage
 ALTER TABLE food_orders ADD COLUMN Commission_Percentage FLOAT;
 UPDATE food_orders SET Commission_Percentage=Commission_Fee/Order_Value ;
-
-
--- R E P E A T     C U S T O M E R S
--- SELECT Customer_ID, COUNT(*) AS Repeat_Orders FROM food_orders GROUP BY Customer_ID ORDER BY COUNT(*) DESC;
--- Since, no. of rows returned is 947, thus we have 51 Repeat Customers w/ 2  of them having ordered thrice and the rest of them having ordered twice
-
--- Order these by how many used the new users discount
--- SELECT * FROM food_orders WHERE Discount_Type LIKE 'NEW USER';
-
-
-
-
